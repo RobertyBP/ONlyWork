c9002dc0d974a41dfac2ecb491aca8ea39dce51e
RobertyBP <54294559+RobertyBP@users.noreply.github.com>
2023-05-16 19:48:28 -0300

    Update index.php

--- webservidor/index.php
+++ webservidor/index.php
@@ -11,34 +11,21 @@
         $acao = "logar";
     }
     $vSessao = $_SESSION["logado"] ?? null;
-    require("./views/header.view.php");
+    require("./views/header_view.php");
     
     if(isset($_SESSION) && $vSessao == "true"){
         if($acao == 'cadastrotrabalho'){
             require('./views/cadastrotrabalho.view.php');
         }else if($acao == 'listartrabalhos'){
-            require('./views/trabalhos.view.php');
+            require('./views/trabalhos_view.php');
         }else{
-            require('./views/trabalhos.view.php');
+            require('./views/trabalhos_view.php');
         }
     }else if ($acao == "cadastrar"){
-        require('./views/cadastrousuario.view.php');
+        require('./views/cadastrousuario_view.php');
     }else if($acao == "logar"){
-        require('./views/login.view.php');
+        require('./views/login_view.php');
     }
-    $rotas =[
-        "\trabalho" => "views\trabalhos_views.php",
-        "\usuario" => "views\usuario_views.php",
-        "\caduser" => "views\cadastrousuario_views.php",
-        "\cadtrabalho" => "views\cadastrotrabalho_views.php",
-        "\login" => "views\login_views.php"
-    ];
-    $url = $_SERVER["REQUEST_URI"];
-    if(array_key_exists($url, $rotas)):
-        requere($rotas[$url]);
-    else:
-        echo "  Erro 404! Página não existe ";
-    endif;
 ?>
 
     
